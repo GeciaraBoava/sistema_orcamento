aaab7ea714f2e5574e857ce21303f06745307308
GeciaraBoava <geciaracardoso@gmail.com>
2025-03-27 21:09:38 -0300

    Criação de DTOs e mappers

--- src/main/java/com/geciara/orcamento/controller/MaterialController.java
+++ src/main/java/com/geciara/orcamento/controller/MaterialController.java
@@ -1,18 +1,15 @@
 package com.geciara.orcamento.controller;
 
 import com.geciara.orcamento.model.entitys.Material;
-import com.geciara.orcamento.services.MaterialService;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.geciara.orcamento.model.services.MaterialService;
 
-@RestController
-@RequestMapping("/api/materiais")  // Define o caminho base para os endpoints
-@Tag(name = "Materiais", description = "Operações relacionadas a materiais")
 public class MaterialController extends GenericController<Material, MaterialService> {
+
+    private final MaterialService materialService;
 
     public MaterialController(MaterialService materialService) {
         super(materialService);
+        this.materialService = materialService;
     }
 
 }
